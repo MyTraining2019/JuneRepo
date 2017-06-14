60d9f4d76a25c034553e8631addb51138e41f36a
Training2017 <captrainingfeb2016@gmail.com>
2017-06-14 17:42:36 +0530

    First Commit

---
+++ Day3-Demo/src/main/java/org/cap/demo/BootClass.java
@@ -0,0 +1,17 @@
+package org.cap.demo;
+
+public class BootClass {
+
+	public static void main(String[] args) {
+		
+		SalesClass class1=new SalesClass();
+		class1.printData();
+		greeting();
+
+	}
+
+	
+	public static void greeting(){
+		System.out.println("Hello! Every One!");
+	}
+}
---
+++ Day3-Demo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class SalesClass {
+	private int[] arr={1,2,3,4,65};
+	
+	public void printData(){
+		
+		System.out.println("Array Elements:");
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+
+}
