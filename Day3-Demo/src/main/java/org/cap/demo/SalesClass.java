6d4b98343ca638c9505f86e241863cd3e07beff8
Training2017 <captrainingfeb2016@gmail.com>
2017-06-14 19:28:01 +0530

    printData method updated

--- Day3-Demo/src/main/java/org/cap/demo/SalesClass.java
+++ Day3-Demo/src/main/java/org/cap/demo/SalesClass.java
@@ -4,10 +4,13 @@ public class SalesClass {
 	private int[] arr={1,2,3,4,65};
 	
 	public void printData(){
-		
+		int sum=0;
 		System.out.println("Array Elements:");
-		for(int i:arr)
-			System.out.println(i);
+		for(int i=0;i<arr.length;i++){
+			sum+=arr[i];
+			System.out.println(arr[i]);
+		}
+		System.out.println("Sum of Array:" + sum);	
 	}
 
 }
